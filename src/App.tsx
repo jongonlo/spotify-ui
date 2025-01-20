e8fa3344f03b49c74b01da2b92bbd2307f88a6cf
Jonathan Gonzalez <jonathanben19962011@gmail.com>
2025-01-19 20:01:36 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { IsrcSearch } from './components/IsrcSearch'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='container text-center justify-self-center my-32'>
       <IsrcSearch />
